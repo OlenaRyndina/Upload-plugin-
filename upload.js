50d6242bd6385480659fb3c33f4804110fa3f2e3
Olena Ryndina <olenaryndina1111@gmail.com>
2022-05-17 18:42:39 +0300

    button upload

--- upload.js
+++ upload.js
@@ -7,14 +7,30 @@ function bytesToSize(bytes) {
 	return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i];
 }
 
-export function upload (selector, options = {}) {
-	const input = document.querySelector(selector);
-	const preview = document.createElement('div');
-	preview.classList.add('preview');
+const element = (tag, classes = [], content) => {
 
-	const open = document.createElement('button');
-	open.classList.add('btn');
-	open.textContent = 'Открыть';
+	const node = document.createElement(tag);
+
+	if(classes.length) {
+		node.classList.add(...classes);
+	}
+
+	if(content) {
+		node.textContent = content;
+	}
+	return node;
+}
+
+function noop() {}
+
+export function upload (selector, options = {}) {
+	let files = [];
+	const onUpload = options.onUpload ?? noop;
+	const input = document.querySelector(selector);
+	const preview = element('div', ['preview']);
+	const open = element('button', ['btn'], 'Открыть');
+	const upload = element('button', ['btn', 'primary'], 'Загрузить');
+	upload.style.display = 'none';
 
 	if (options.multi) {
 		input.setAttribute('multiple', true);
@@ -25,6 +41,7 @@ export function upload (selector, options = {}) {
 	}
 
     input.insertAdjacentElement('afterend', preview);
+    input.insertAdjacentElement('afterend', upload);
 	input.insertAdjacentElement('afterend', open);
 
 	const triggerInput = () => input.click();
@@ -34,8 +51,9 @@ export function upload (selector, options = {}) {
     		return
     	}
 
-    	const files = Array.from(event.target.files);
+    	files = Array.from(event.target.files);
         preview.innerHTML = '';
+        upload.style.display = 'inline';
     	files.forEach((file) => {
     		if(!file.type.match('image')) {
     			return
@@ -47,7 +65,7 @@ export function upload (selector, options = {}) {
     			const src = e.target.result;
     			preview.insertAdjacentHTML('afterbegin', `
     				<div class = "preview-image">
-    				    <div class = "preview-remove">&times;</div>
+    				    <div class = "preview-remove" data-name = "${file.name}">&times;</div>
                         <img src = "${src}" alt = "${file.name}">
                         <div class = "preview-info">
                             <span>${file.name}</span>
@@ -61,7 +79,41 @@ export function upload (selector, options = {}) {
     	})
     }
 
+    const removeHandler = (e) => {
+    	if(!e.target.dataset.name) {
+    		return
+    	}
+
+    	const {name} = e.target.dataset;
+    	files = files.filter(file => file.name !== name);
+
+    	if(!files.length) {
+    		upload.style.display = 'none';
+    	}
+
+    	const block = preview
+    	    .querySelector(`[data-name="${name}"]`)
+    	    .closest('.preview-image');
+
+    	block.classList.add('removing');
+    	setTimeout(() => block.remove(), 300)
+    }
+
+    const clearPreview = (el) => {
+    	el.style.bottom = '4px';
+        el.innerHTML = '<div class = "preview-info-progress"></div>'
+    }
+
+    const uploadHandler = () => {
+    	preview.querySelectorAll('.preview-remove').forEach(e => e.remove());
+    	const previewInfo = preview.querySelectorAll('.preview-info');
+    	previewInfo.forEach(clearPreview);
+        onUpload(files);
+    }
+
 	open.addEventListener('click', triggerInput);
 	input.addEventListener('change', changeHandler);
+	preview.addEventListener('click', removeHandler);
+	upload.addEventListener('click', uploadHandler);
 }
 
